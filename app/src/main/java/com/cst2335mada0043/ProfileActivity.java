cc94065f600f05e1b52feb5d106fc9ae73cda95f
amalr <arjmr2000>
2021-02-11 23:16:33 -0500

    finished lab3

---
+++ app/src/main/java/com/cst2335mada0043/ProfileActivity.java
@@ -0,0 +1,85 @@
+package com.cst2335mada0043;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class ProfileActivity extends AppCompatActivity {
+    private static final String ACTIVITY_NAME="activity_profile";
+    static final int REQUEST_IMAGE_CAPTURE=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        EditText emailEditText = (EditText) findViewById(R.id.EditTxt2);
+
+        Intent fromMain = getIntent();
+        String recievedEmail = fromMain.getStringExtra("EMAIL");
+        emailEditText.setText(recievedEmail);
+
+
+        ImageButton takePictureBtn = (ImageButton) findViewById(R.id.imgbutton);
+        takePictureBtn.setOnClickListener(c -> {
+
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageButton takePictureBtn = (ImageButton) findViewById(R.id.imgbutton);
+            takePictureBtn.setImageBitmap(imageBitmap);
+        }
+        Log.d(ACTIVITY_NAME, "In function: onActivityResult()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ACTIVITY_NAME, "In function: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ACTIVITY_NAME, "In function: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ACTIVITY_NAME, "In function: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ACTIVITY_NAME, "In function: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(ACTIVITY_NAME, "In function: onDestroy()");
+    }
+}
