e1856e9d36d8d51d6a4cb55d42eb7ddecd81b64c
amalr <arjmr2000>
2021-03-25 23:34:42 -0400

    Lab7

---
+++ app/src/main/java/com/cst2335/cst2335mada0043/activity_main_login.java
@@ -0,0 +1,47 @@
+package com.cst2335.cst2335mada0043;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class activity_main_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+
+        EditText emailField = (EditText) findViewById(R.id.editText);
+        //  EditText passField=(EditText) findViewById(R.id.editView3);
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String savedString = preferences.getString("email", "");
+        emailField.setText(savedString);
+
+        //getting the login button with ClickListner function
+        Button loginBtn = (Button) findViewById(R.id.button1);
+        loginBtn.setOnClickListener( c -> {
+
+            Intent profilePage = new Intent(activity_main_login.this, activity_profile.class);
+            profilePage.putExtra("EMAIL", emailField.getText().toString().trim());
+            startActivityForResult( profilePage, 345);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EditText emailField = (EditText) findViewById(R.id.editText);
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        String textEmailAddress=emailField.getText().toString().trim();
+        editor.putString("email", textEmailAddress);
+        editor.commit();
+        editor.apply();
+
+    }
+}
---
+++ app/src/main/java/com/cst2335/cst2335mada0043/activity_profile.java
@@ -0,0 +1,97 @@
+package com.cst2335.cst2335mada0043;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class activity_profile extends AppCompatActivity {
+    private static final String ACTIVITY_NAME="activity_profile";
+    static final int REQUEST_IMAGE_CAPTURE=1;
+
+   // @Override
+   // protected void onCreate(Bundle savedInstanceState) {
+     //   super.onCreate(savedInstanceState);
+      // setContentView(R.layout.activity_profile);
+
+       // String email = getIntent().getStringExtra("EMAIL");
+       // EditText emailField = findViewById(R.id.editText1);
+        //emailField.setText(email);
+    //}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        EditText emailEditText=(EditText)findViewById(R.id.editText1) ;
+
+        Intent fromMain=getIntent();
+        String recievedEmail= fromMain.getStringExtra("EMAIL");
+        emailEditText.setText(recievedEmail);
+
+
+        ImageButton takePictureBtn = (ImageButton) findViewById(R.id.button2);
+        takePictureBtn.setOnClickListener(c -> {
+
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        });
+        Button ChatButton = (Button) findViewById(R.id.button3);
+        ChatButton.setOnClickListener( c -> {
+
+            Intent goToProfile  = new Intent(activity_profile.this, ChatRoomActivity.class);
+            startActivityForResult( goToProfile,345);
+        });
+        Button WeatherButton = (Button) findViewById(R.id.button4);
+        WeatherButton.setOnClickListener( c -> {
+
+            Intent goToWeather  = new Intent(activity_profile.this, WeatherForecast.class);
+            startActivityForResult( goToWeather,345);
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageButton takePictureBtn = (ImageButton) findViewById(R.id.button2);
+            takePictureBtn.setImageBitmap(imageBitmap);
+        }
+        Log.d(ACTIVITY_NAME, "In function: onActivityResult()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ACTIVITY_NAME, "In function: onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ACTIVITY_NAME, "In function: onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ACTIVITY_NAME, "In function: onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ACTIVITY_NAME, "In function: onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(ACTIVITY_NAME, "In function: onDestroy()");
+    }
+}
